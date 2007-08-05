1bdf4111fffb9ba6f733293f0c7da6c4ef3f2d5d
mpenning <none@none>
2007-08-05 12:46:14 -0500

    [svn r28] Completed unit tests
    --HG--
    branch : ciscoconfparse

--- ciscoconfparse/testCiscoConfParse.py
+++ ciscoconfparse/testCiscoConfParse.py
@@ -21,8 +21,48 @@ class knownValues(unittest.TestCase):
    ' encapsulation ppp',
    ' ip address 1.1.1.1 255.255.255.252',
    '!',
+   'interface GigabitEthernet4/1',
+   ' switchport',
+   ' switchport access vlan 100',
+   ' switchport voice vlan 150',
+   ' power inline static max 7000',
+   '!',
+   'interface GigabitEthernet4/2',
+   ' switchport',
+   ' switchport access vlan 100',
+   ' switchport voice vlan 150',
+   ' power inline static max 7000',
+   '!',
+   'interface GigabitEthernet4/3',
+   ' switchport',
+   ' switchport access vlan 100',
+   ' switchport voice vlan 150',
+   '!',
+   'interface GigabitEthernet4/4',
+   ' shutdown',
+   '!',
+   'interface GigabitEthernet4/5',
+   ' switchport',
+   ' switchport access vlan 110',
+   '!',
+   'interface GigabitEthernet4/6',
+   ' switchport',
+   ' switchport access vlan 110',
+   '!',
+   'interface GigabitEthernet4/7',
+   ' switchport',
+   ' switchport access vlan 110',
+   '!',
+   'interface GigabitEthernet4/8',
+   ' switchport',
+   ' switchport access vlan 110',
+   '!',
    'access-list 101 deny tcp any any eq 25 log',
    'access-list 101 permit ip any any',
+   '!',
+   '!',
+   'logging 1.1.3.5',
+   'logging 1.1.3.17',
    '!',
    'banner login ^C'
    'This is a router, and you cannot have it.',
@@ -55,16 +95,55 @@ class knownValues(unittest.TestCase):
    ]
 
 
+   c01_parents_w_child_power = [
+   'interface GigabitEthernet4/1',
+   'interface GigabitEthernet4/2',
+   ]
+
+
+   c01_parents_w_child_voice = [
+   'interface GigabitEthernet4/1',
+   'interface GigabitEthernet4/2',
+   'interface GigabitEthernet4/3',
+   ]
+
+   c01_parents_wo_child_power = [
+   'interface GigabitEthernet4/3',
+   'interface GigabitEthernet4/4',
+   'interface GigabitEthernet4/5',
+   'interface GigabitEthernet4/6',
+   'interface GigabitEthernet4/7',
+   'interface GigabitEthernet4/8',
+   ]
+
+
+   c01_req_excl_logging = [
+   'no logging 1.1.3.17',
+   'logging 1.1.3.4',
+   'logging 1.1.3.6',
+   ]
+
+   c01_req_all_logging = [
+   'logging 1.1.3.4',
+   'logging 1.1.3.6',
+   ]
+
    c02 = [
    'interface Serial1/0',
    ' encapsulation ppp',
    ' ip address 1.1.1.1 255.255.255.252',
    ]
 
+   c02_encap = [
+   ' encapsulation ppp',
+   ]
+
    #--------------------------------
 
    find_lines_Values = ( 
-   ( c01, [ "interface", False ], c01_intf ),
+   ( c02, [ "encapsulation", False ], c02_encap ),
+   ( c01, [ "interface Serial 1/0", False], c01_intf ),
+   ( c01, [ "interface Serial1/0", True], False ),
    )
 
    find_children_Values = ( 
@@ -75,6 +154,16 @@ class knownValues(unittest.TestCase):
    find_all_children_Values = ( 
    ( c01, [ "policy-map", False ], c01_pmap_all_children ),
    ( c01, [ "policy-map", True ], False),
+   )
+
+   find_parents_w_child_Values = (
+   ( c01, [ "interf", "power inline" ], c01_parents_w_child_power ),
+   ( c01, [ "interf", " switchport voice" ], c01_parents_w_child_voice ),
+   ( c01, [ "^interface$", "switchport voice" ], False ),
+   )
+
+   find_parents_wo_child_Values = (
+   ( c01, [ "interface Gig", "power inline" ], c01_parents_wo_child_power ),
    )
 
    #--------------------------------
@@ -98,6 +187,48 @@ class knownValues(unittest.TestCase):
          cfg = CiscoConfParse( config )
          result = cfg.find_all_children( args[0], args[1] )
          self.assertEqual( resultGood, result )
+
+
+   def testValues_find_parents_w_child(self):
+      for config, args, resultGood in self.find_parents_w_child_Values:
+         cfg = CiscoConfParse( config )
+         result = cfg.find_parents_w_child( args[0], args[1] )
+         self.assertEqual( resultGood, result )
+
+
+   def testValues_find_parents_wo_child(self):
+      for config, args, resultGood in self.find_parents_wo_child_Values:
+         cfg = CiscoConfParse( config )
+         result = cfg.find_parents_wo_child( args[0], args[1] )
+         self.assertEqual( resultGood, result )
+
+   def testValues_req_cfgspec_excl_diff(self):
+      resultGood = self.c01_req_excl_logging
+      cfg = CiscoConfParse( self.c01 )
+      result = cfg.req_cfgspec_excl_diff( 
+          "^logging\s+",
+          "logging\s+\d+\.\d+\.\d+\.\d+",
+         [
+         'logging 1.1.3.4',
+         'logging 1.1.3.5',
+         'logging 1.1.3.6',
+         ]
+	 )
+      self.assertEqual( resultGood, result )
+
+
+   def testValues_req_cfgspec_all_diff(self):
+      resultGood = self.c01_req_all_logging
+      cfg = CiscoConfParse( self.c01 )
+      result = cfg.req_cfgspec_all_diff( 
+         [
+         'logging 1.1.3.4',
+         'logging 1.1.3.5',
+         'logging 1.1.3.6',
+         ]
+	 )
+      self.assertEqual( resultGood, result )
+
 
 
 if __name__ == "__main__":
