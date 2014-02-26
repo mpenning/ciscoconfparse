c26c80ee4ac7ca46bed5346d7cefeb9c542175aa
mpenning <mike@pennington.net>
2014-02-26 06:42:31 -0600

    Improve build workflow
    --HG--
    branch : ciscoconfparse

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(name='ciscoconfparse',
-      version='0.9.19',
+      version=__import__("ciscoconfparse").__version__,
       description='Parse, Audit, Query, Build, and Modify Cisco IOS-style configurations',
       url='http://www.pennington.net/py/ciscoconfparse/',
       author='David Michael Pennington',
