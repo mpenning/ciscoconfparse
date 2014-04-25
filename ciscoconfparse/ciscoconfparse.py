923879a4fb1186d32dc7e3777adbe76b8e97d05e
mpenning <mike@pennington.net>
2014-04-25 05:49:02 -0500

    Roll version number
    --HG--
    branch : ciscoconfparse

--- ciscoconfparse/ciscoconfparse.py
+++ ciscoconfparse/ciscoconfparse.py
@@ -40,7 +40,7 @@ except ImportError:
 """
 
 ## Docstring props: http://stackoverflow.com/a/1523456/667301
-__version_tuple__ = (0,9,32)
+__version_tuple__ = (0,9,33)
 __version__ = '.'.join(map(str, __version_tuple__))
 __email__ = "mike /at\ pennington [dot] net"
 __author__ = "David Michael Pennington <{0}>".format(__email__)
