65ca5215aff55f6ad8f4550bce94e2bee189aea7
mpenning <mike@pennington.net>
2014-07-26 16:15:29 -0500

    try this
    --HG--
    branch : ciscoconfparse

--- setup.py
+++ setup.py
@@ -6,23 +6,26 @@ import sys
 sys.path.insert(0,os.getcwd()+'/ciscoconfparse')
 
 def read(fname):
+    # Dynamically generate setup(long_description)
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(name='ciscoconfparse',
       version=__import__("ciscoconfparse").__version__,
-      description='Parse, Audit, Query, Build, and Modify Cisco IOS-style configurations',
-      url='http://www.pennington.net/py/ciscoconfparse/',
+      url='http://github.com/mpenning/ciscoconfparse',
       author='David Michael Pennington',
       author_email='mike /|at|\ pennington.net',
       license='GPL',
+      include_package_data=True,
+      zip_safe=False,
       platforms='any',
       keywords='Parse audit query modify Cisco IOS configuration',
-      entry_points = "",
+      description='Parse, Audit, Query, Build, and Modify Cisco IOS-style configurations',
       long_description=read('README.rst'),
-      include_package_data=True,
-      #packages=find_packages(),
-      packages=['ciscoconfparse'],
-      zip_safe=False,
+      packages=find_packages('ciscoconfparse'),
+      package_dir = {'': 'ciscoconfparse'},
+      #namespace_packages=['ciscoconfparse'],
+      #py_modules=['ciscoconfparse'],
+      use_2to3=False,
       setup_requires=["setuptools_hg"],  # setuptools_hg must be installed as a python module
       classifiers=[
           'Development Status :: 5 - Production/Stable',
