37d2212d9f558d5deac5a7b48ce3b80c4c4ae6a1
mpenning <mike@pennington.net>
2014-09-18 06:33:21 -0500

    Roll version number
    --HG--
    branch : ciscoconfparse

--- ciscoconfparse/ciscoconfparse.py
+++ ciscoconfparse/ciscoconfparse.py
@@ -39,7 +39,7 @@ from models_cisco import IOSCfgLine
 """
 
 ## Docstring props: http://stackoverflow.com/a/1523456/667301
-__version_tuple__ = (1,1,10)
+__version_tuple__ = (1,1,11)
 __version__ = '.'.join(map(str, __version_tuple__))
 __email__ = "mike /at\ pennington [dot] net"
 __author__ = "David Michael Pennington <{0}>".format(__email__)
