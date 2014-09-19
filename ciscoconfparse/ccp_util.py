8a1513925ebfa66ec97453b23a275c3c925ad6ee
mpenning <mike@pennington.net>
2014-09-19 03:41:23 -0500

    Add copyright text and enhance automatic IOS-style addr / mask parsing
    --HG--
    branch : ciscoconfparse

--- ciscoconfparse/ccp_util.py
+++ ciscoconfparse/ccp_util.py
@@ -21,14 +21,40 @@ except:
         # Relative import path referenced to this directory
         from ipaddress import IPv4Network, IPv6Network, IPv4Address, IPv6Address
 
+""" ccp_util.py - Parse, Query, Build, and Modify IOS-style configurations
+     Copyright (C) 2007-2014 David Michael Pennington
+
+     This program is free software: you can redistribute it and/or modify
+     it under the terms of the GNU General Public License as published by
+     the Free Software Foundation, either version 3 of the License, or
+     (at your option) any later version.
+
+     This program is distributed in the hope that it will be useful,
+     but WITHOUT ANY WARRANTY; without even the implied warranty of
+     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+     GNU General Public License for more details.
+
+     You should have received a copy of the GNU General Public License
+     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+     If you need to contact the author, you can do so by emailing:
+     mike [~at~] pennington [/dot\] net
+"""
+
+
+RGX_IPV4ADDR = re.compile(r'^(\d+\.\d+\.\d+\.\d+)')
+RGX_IPV4ADDR_NETMASK = re.compile(r'(\d+\.\d+\.\d+\.\d+)\s+(\d+\.\d+\.\d+\.\d+)')
+
 ## Emulate the old behavior of ipaddr.IPv4Network in Python2, which can use
 ##    IPv4Network with a host address.  Google removed that in Python3's 
 ##    ipaddress.py module
 class IPv4Obj(object):
-    def __init__(self, arg='127.0.0.1/32', strict=False):
+    def __init__(self, arg='127.0.0.1/32', strict=False, 
+        RGX_IPV4ADDR=RGX_IPV4ADDR, RGX_IPV4ADDR_NETMASK=RGX_IPV4ADDR_NETMASK):
+        arg = RGX_IPV4ADDR_NETMASK.sub(r'\1/\2', arg) # mangle IOS: 'addr mask'
         self.arg = arg
-        mm = re.search(r'^(\d+\.\d+\.\d+\.\d+)', arg)
-        assert (not (mm is None))
+        mm = RGX_IPV4ADDR.search(arg)
+        assert (not (mm is None)), "IPv4Obj couldn't parse {0}".format(arg)
         self.network_object = IPv4Network(arg, strict=strict)
         self.ip_object = IPv4Address(mm.group(1))
 
