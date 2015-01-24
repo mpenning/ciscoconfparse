40e2c136ade747d93fe3d1af3d3fa10515ee3719
mpenning <mike@pennington.net>
2015-01-24 11:20:50 -0600

    Lower time between tests
    --HG--
    branch : ciscoconfparse

--- ciscoconfparse/runtests.sh
+++ ciscoconfparse/runtests.sh
@@ -2,9 +2,9 @@
 
 # Change into your virtualenv before running the script
 python test_CiscoConfParse.py
-sleep 1
+sleep 0.25
 python test_Ccp_Util.py
-sleep 1
+sleep 0.25
 python test_Models_Cisco.py
-sleep 1
+sleep 0.25
 python test_Models_Asa.py
