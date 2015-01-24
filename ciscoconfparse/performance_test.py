efa63dc744529e20bc84acaed53ef6430dd03815
mpenning <mike@pennington.net>
2015-01-24 11:20:28 -0600

    add new performance tests
    --HG--
    branch : ciscoconfparse

--- ciscoconfparse/performance_test.py
+++ ciscoconfparse/performance_test.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python
 
 from cProfile import run
+import sys
+
 from ciscoconfparse import CiscoConfParse
 
-# python -m cProfile -s time test_speed.py
-run("CiscoConfParse('../configs/sample_05.ios')", sort=1)
+if sys.argv[1]=="1":
+    run("CiscoConfParse('../configs/sample_01.ios')", sort=2)
+elif sys.argv[1]=="5":
+    # python -m cProfile -s time test_speed.py
+    run("CiscoConfParse('../configs/sample_05.ios')", sort=2)
+elif sys.argv[1]=="6":
+    # python -m cProfile -s time test_speed.py
+    run("CiscoConfParse('../configs/sample_06.ios', syntax='ios', factory=True)", sort=2)
+else:
+    raise ValueError
