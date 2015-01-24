fccf0582e1e0b640ea031e1a1f4743ad727d2127
mpenning <mike@pennington.net>
2015-01-24 13:10:31 -0600

    add more test options
    --HG--
    branch : ciscoconfparse

--- ciscoconfparse/parse_test.py
+++ ciscoconfparse/parse_test.py
@@ -1,5 +1,11 @@
 from ciscoconfparse import CiscoConfParse
+import sys
 
-parse = CiscoConfParse('../configs/sample_01.ios', syntax='ios', factory=True)
-for line in parse.objs:
-    print line
+if sys.argv[1]=="1":
+    parse = CiscoConfParse('../configs/sample_01.ios', syntax='ios', factory=False)
+    for line in parse.objs:
+        print line
+elif sys.argv[1]=="2":
+    parse = CiscoConfParse('../configs/sample_01.ios', syntax='ios', factory=True)
+    for line in parse.objs:
+        print line
