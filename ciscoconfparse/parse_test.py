1cf80eb714793e309fc98d2955415421e846b10f
mpenning <mike@pennington.net>
2015-01-25 07:02:27 -0600

    Add IOS-XR banner parse
    --HG--
    branch : ciscoconfparse

--- ciscoconfparse/parse_test.py
+++ ciscoconfparse/parse_test.py
@@ -9,3 +9,7 @@ elif sys.argv[1]=="2":
     parse = CiscoConfParse('../configs/sample_01.ios', syntax='ios', factory=True)
     for line in parse.objs:
         print line
+elif sys.argv[1]=="3":
+    parse = CiscoConfParse('../configs/sample_01.iosxr', syntax='ios', factory=True)
+    for line in parse.objs:
+        print line
