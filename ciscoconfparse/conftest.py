0af48ea866318914c2abd2ac21ed2fc506f974f7
mpenning <mike@pennington.net>
2015-03-02 03:40:04 -0600

    Add pytest support file
    --HG--
    branch : ciscoconfparse

---
+++ ciscoconfparse/conftest.py
@@ -0,0 +1,234 @@
+import sys
+sys.path.insert(0, './')
+
+import pytest
+from .ciscoconfparse import CiscoConfParse
+
+
+c01 = """policy-map QOS_1
+ class GOLD
+  priority percent 10
+ !
+ class SILVER
+  bandwidth 30
+  random-detect
+ !
+ class BRONZE
+  random-detect
+!
+interface Serial 1/0
+ encapsulation ppp
+ ip address 1.1.1.1 255.255.255.252
+!
+interface GigabitEthernet4/1
+ switchport
+ switchport access vlan 100
+ switchport voice vlan 150
+ power inline static max 7000
+!
+interface GigabitEthernet4/2
+ switchport
+ switchport access vlan 100
+ switchport voice vlan 150
+ power inline static max 7000
+!
+interface GigabitEthernet4/3
+ switchport
+ switchport access vlan 100
+ switchport voice vlan 150
+!
+interface GigabitEthernet4/4
+ shutdown
+!
+interface GigabitEthernet4/5
+ switchport
+ switchport access vlan 110
+!
+interface GigabitEthernet4/6
+ switchport
+ switchport access vlan 110
+!
+interface GigabitEthernet4/7
+ switchport
+ switchport access vlan 110
+!
+interface GigabitEthernet4/8
+ switchport
+ switchport access vlan 110
+!
+access-list 101 deny tcp any any eq 25 log
+access-list 101 permit ip any any
+!
+!
+logging 1.1.3.5
+logging 1.1.3.17
+!
+banner login ^C
+This is a router, and you cannot have it.
+Log off now while you still can type. I break the fingers
+of all tresspassers.
+^C
+alias exec showthang show ip route vrf THANG""".splitlines()
+
+config_c01_default_gige = """policy-map QOS_1
+ class GOLD
+  priority percent 10
+ !
+ class SILVER
+  bandwidth 30
+  random-detect
+ !
+ class BRONZE
+  random-detect
+!
+interface Serial 1/0
+ encapsulation ppp
+ ip address 1.1.1.1 255.255.255.252
+!
+default interface GigabitEthernet4/1
+interface GigabitEthernet4/1
+ switchport
+ switchport access vlan 100
+ switchport voice vlan 150
+ power inline static max 7000
+!
+default interface GigabitEthernet4/2
+interface GigabitEthernet4/2
+ switchport
+ switchport access vlan 100
+ switchport voice vlan 150
+ power inline static max 7000
+!
+default interface GigabitEthernet4/3
+interface GigabitEthernet4/3
+ switchport
+ switchport access vlan 100
+ switchport voice vlan 150
+!
+default interface GigabitEthernet4/4
+interface GigabitEthernet4/4
+ shutdown
+!
+default interface GigabitEthernet4/5
+interface GigabitEthernet4/5
+ switchport
+ switchport access vlan 110
+!
+default interface GigabitEthernet4/6
+interface GigabitEthernet4/6
+ switchport
+ switchport access vlan 110
+!
+default interface GigabitEthernet4/7
+interface GigabitEthernet4/7
+ switchport
+ switchport access vlan 110
+!
+default interface GigabitEthernet4/8
+interface GigabitEthernet4/8
+ switchport
+ switchport access vlan 110
+!
+access-list 101 deny tcp any any eq 25 log
+access-list 101 permit ip any any
+!
+!
+logging 1.1.3.5
+logging 1.1.3.17
+!
+banner login ^C
+This is a router, and you cannot have it.
+Log off now while you still can type. I break the fingers
+of all tresspassers.
+^C
+alias exec showthang show ip route vrf THANG""".splitlines()
+
+config_c01_insert_serial_replace = """policy-map QOS_1
+ class GOLD
+  priority percent 10
+ !
+ class SILVER
+  bandwidth 30
+  random-detect
+ !
+ class BRONZE
+  random-detect
+!
+default interface Serial 2/0
+interface Serial 2/0
+ encapsulation ppp
+ ip address 1.1.1.1 255.255.255.252
+!
+interface GigabitEthernet4/1
+ switchport
+ switchport access vlan 100
+ switchport voice vlan 150
+ power inline static max 7000
+!
+interface GigabitEthernet4/2
+ switchport
+ switchport access vlan 100
+ switchport voice vlan 150
+ power inline static max 7000
+!
+interface GigabitEthernet4/3
+ switchport
+ switchport access vlan 100
+ switchport voice vlan 150
+!
+interface GigabitEthernet4/4
+ shutdown
+!
+interface GigabitEthernet4/5
+ switchport
+ switchport access vlan 110
+!
+interface GigabitEthernet4/6
+ switchport
+ switchport access vlan 110
+!
+interface GigabitEthernet4/7
+ switchport
+ switchport access vlan 110
+!
+interface GigabitEthernet4/8
+ switchport
+ switchport access vlan 110
+!
+access-list 101 deny tcp any any eq 25 log
+access-list 101 permit ip any any
+!
+!
+logging 1.1.3.5
+logging 1.1.3.17
+!
+banner login ^C
+This is a router, and you cannot have it.
+Log off now while you still can type. I break the fingers
+of all tresspassers.
+^C
+alias exec showthang show ip route vrf THANG""".splitlines()
+
+@pytest.yield_fixture(scope='session')
+def c01_default_gigethernets(request):
+    yield config_c01_default_gige
+
+@pytest.yield_fixture(scope='session')
+def c01_insert_serial_replace(request):
+    yield config_c01_insert_serial_replace
+
+@pytest.yield_fixture(scope='function')
+def parse_c01(request):
+    """Preparsed c01"""
+    parse_c01 = CiscoConfParse(c01, factory=False)
+     
+    yield parse_c01
+
+@pytest.yield_fixture(scope='function')
+def parse_c01_factory(request):
+    """Preparsed c01 with factory option"""
+    parse_c01_factory = CiscoConfParse(c01, factory=True)
+     
+    yield parse_c01_factory
+
+
