7827a22e59a39fe39657a90399a15ae6095a1314
mpenning <mike@pennington.net>
2015-06-13 11:35:00 -0500

    Shorten delay between unit tests
    --HG--
    branch : ciscoconfparse

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -2,11 +2,11 @@
 
 # Change into your virtualenv before running the script
 py.test -s test_CiscoConfParse.py
-sleep 0.25
+sleep 0.05
 py.test -s test_Ccp_Util.py
-sleep 0.25
+sleep 0.05
 py.test -s test_Models_Cisco.py
-sleep 0.25
+sleep 0.05
 py.test -s test_Models_Asa.py
-sleep 0.25
+sleep 0.05
 py.test -s test_Models_Junos.py
