098627341d35c26889322569f134c1f38a9fb1e0
Steffen Schmidt <service.github@nocer.net>
2016-06-24 22:10:23 +0200

    Fixed wait afer each test suite

--- tests/runtests.cmd
+++ tests/runtests.cmd
@@ -1,13 +1,12 @@
 @echo off
 @cls
-REM Change into your virtualenv before running the script
 py.test -s test_CiscoConfParse.py
-sleep 1
+ping -n 1 127.0.0.1 > NUL
 py.test -s test_Ccp_Util.py
-sleep 1
+ping -n 1 127.0.0.1 > NUL
 py.test -s test_Models_Cisco.py
-sleep 1
+ping -n 1 127.0.0.1 > NUL
 py.test -s test_Models_Asa.py
-sleep 1
+ping -n 1 127.0.0.1 > NUL
 py.test -s test_Models_Junos.py
 pause
