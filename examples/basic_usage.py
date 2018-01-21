b33bdc6c6a10a2901945528373ceae3aaad95a13
mpenning <mike@pennington.net>
2018-01-21 17:05:58 -0600

    Update with recommended syntax

--- examples/basic_usage.py
+++ examples/basic_usage.py
@@ -1,14 +1,42 @@
 #!/usr/bin/env python
 from ciscoconfparse import CiscoConfParse
 
-parse = CiscoConfParse("../configs/sample_01.ios")
+CONFIG = """!
+interface Serial 1/0
+ encapsulation ppp
+ ip address 1.1.1.1 255.255.255.252
+!
+interface GigabitEthernet4/1
+ switchport
+ switchport access vlan 100
+ switchport voice vlan 150
+ power inline static max 7000
+!
+interface GigabitEthernet4/2
+ switchport
+ switchport access vlan 100
+ switchport voice vlan 150
+ power inline static max 7000
+!
+interface GigabitEthernet4/3
+ switchport
+ switchport access vlan 100
+ switchport voice vlan 150
+ shutdown
+!"""
 
-# Return a list of all ATM interfaces and subinterfaces
-atm_intfs = parse.find_lines("^interface\sATM")
 
-# Return a list of all interfaces with a certain QOS policy
-qos_intfs = parse.find_parents_w_child( "^interf", "service-policy" )
+parse = CiscoConfParse(CONFIG.splitlines())
+
+# Return a list of all interfaces
+intfs = parse.find_objects("^interface")
+
+# Return a list of all interfaces with ppp encap
+ppp_intfs = parse.find_objects_w_child( "^interf", "encapsulation ppp")
 
 # Return a list of all active interfaces (i.e. not shutdown)
-active_intfs = parse.find_parents_wo_child( "^interf", "shutdown" )
+active_intfs = parse.find_objects_wo_child( "^interf", "shutdown" )
+for obj in active_intfs:
+    print "OBJ", obj
+    print "  CONFIG:", obj.text
 
