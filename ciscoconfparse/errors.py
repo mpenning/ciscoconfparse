da558671e6f9fc4112734605e458a6e262150e5b
mpenning <mike@pennington.net>
2018-01-24 05:19:32 -0600

    DynamicAddressException

---
+++ ciscoconfparse/errors.py
@@ -0,0 +1,10 @@
+class BaseError(Exception):
+    def __init__(self, msg=""):
+        super(BaseError, self).__init__(msg)
+        self.msg = msg
+
+class DynamicAddressException(Exception):
+    """Throw this if you try to get an address object from a dhcp interface"""
+    def __init__(self, msg=""):
+        super(DynamicAddressException, self).__init__(msg)
+        self.msg = msg
