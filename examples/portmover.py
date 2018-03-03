35063d5e0474211125e916a2119800c8ec021e5f
mpenning <mike@pennington.net>
2018-03-03 08:11:13 -0600

    Add more documentation; make logic slightly more clear

--- examples/portmover.py
+++ examples/portmover.py
@@ -7,31 +7,47 @@ from ciscoconfparse import CiscoConfParse
 """
 Author: David Michael Pennington <mike@pennington.net>
 License: GPLv3
+
+portmover.py:
+Move ports from an 'old' switch to a new switch:
+    Read a Cisco IOS config and a csv containing port mappings; 
+    output a new switch config
 """
 
 def main(configpath, newconfigpath, csvpath):
-    parse = CiscoConfParse(configpath)
-    config = list()
+    """Read a Cisco IOS config and a csv containing port mappings; output a new config"""
+    parse = CiscoConfParse(configpath) # parse the old config
+    config = list()  # Initialize a container for the new config
+
+    # Open the csv... 
     with open(csvpath) as csvfh:
         reader = csv.reader(csvfh)
+
+        # iterate over the port mappings in the csv...
         for idx, line in enumerate(reader):
             if idx==0:  # Skip the header line
                 continue
             elif line==[]:  # Skip empty lines
                 continue
-            ## Parse out the old interface info from the csv file
+
+            # Ensure there are two columns on this csv line
+            assert len(line)==2, "CSV line: '{0}' must have two columns".format(
+                line)
+
+            ## Read the old & new interface info from the csv file
             oldport, newport = line[0], line[1]
+
+            ## Parse out the interface class & number via regex...
+            mm = re.search(r'^(\w+?)\s*(\d\S+)$', oldport.strip())
+            if mm is not None:
+                # We correctly parsed the old port name into parts
+                oldclass, oldnum = mm.group(1), mm.group(2)
+            else:
+                # We can't parse the old port name into parts...
+                error = "Cannot parse '{0}' in the csv".format(
+                    oldport)
+                raise ValueError(error)
             try:
-                ## Parse out the interface class & number via regex...
-                mm = re.search(r'^(\w+?)\s*(\d\S+?)$', oldport.strip())
-                if mm is not None:
-                    # We correctly parsed the old port name into parts
-                    oldclass, oldnum = mm.group(1), mm.group(2)
-                else:
-                    # We can't parse the old port name into parts...
-                    error = "Cannot parse '{0}' in the csv".format(
-                        oldport)
-                    raise ValueError(error)
                 # Find the old interface line...
                 intfobj = parse.find_objects('^interface\s+{0}\s*{1}'.format(
                     oldclass, oldnum))[0]
@@ -41,17 +57,17 @@ def main(configpath, newconfigpath, csvpath):
 
             ## Default the new interface to prevent any potential problems...
             config.append('default interface {0}'.format(newport))
-            ## Add the new configuration...
+            ## Add the new interface configuration...
             config.append('interface {0}'.format(newport))
-            ## Add all the configuration for this interface...
+            ## Copy all the old configuration to the new interface...
             for child in intfobj.all_children:
                 config.append(child.text)
             config.append('!')
 
-    ## Write the new config
-    with open(newconfigpath, 'w') as outfh:
+    ## Write the new config file
+    with open(newconfigpath, 'w') as newconffh:
         for line in config:
-            outfh.write(line+os.linesep)
+            newconffh.write(line+os.linesep)
 
 if __name__=='__main__':
     """Read a Cisco IOS config and a csv containing port mappings; output a new config"""
