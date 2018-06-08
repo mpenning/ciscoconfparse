3b7ed69ece16b9e46643b0a287427417e8d43034
Matěj Cepl <mcepl@cepl.eu>
2018-06-08 15:11:07 +0200

    Contrite penance: never provide fix without testing.

--- tests/parse_test.py
+++ tests/parse_test.py
@@ -9,12 +9,12 @@ from ciscoconfparse import CiscoConfParse
 if sys.argv[1]=="1":
     parse = CiscoConfParse('../configs/sample_01.ios', syntax='ios', factory=False)
     for line in parse.objs:
-        print line
+        print(line)
 elif sys.argv[1]=="2":
     parse = CiscoConfParse('../configs/sample_01.ios', syntax='ios', factory=True)
     for line in parse.objs:
-        print line
+        print(line)
 elif sys.argv[1]=="3":
     parse = CiscoConfParse('../configs/sample_01.iosxr', syntax='ios', factory=True)
     for line in parse.objs:
-        print line
+        print(line)
