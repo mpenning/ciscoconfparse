a4c732da2d6fd9010858fe449229167eeef5e0e3
mpenning <mike@pennington.net>
2019-06-01 15:38:40 -0500

    Add more examples of finding interface parameters

--- examples/basic_usage.py
+++ examples/basic_usage.py
@@ -36,7 +36,19 @@ ppp_intfs = parse.find_objects_w_child( "^interf", "encapsulation ppp")
 
 # Return a list of all active interfaces (i.e. not shutdown)
 active_intfs = parse.find_objects_wo_child( "^interf", "shutdown" )
-for obj in active_intfs:
-    print "OBJ", obj
-    print "  CONFIG:", obj.text
 
+# For each interface above, print out relevant information...
+for obj in active_intfs:
+    print("-----")
+
+    # Find the interface mode
+    intf_mode = obj.re_match_iter_typed('^\s*(switchport)$', default='routed')
+
+    # Find the IP address by iterating over all children of this parent...
+    ip_addr = obj.re_match_iter_typed('ip\saddress\s(\S+)', default='N/A')
+
+    # Print out what we found...
+    print("Object: {0}".format(obj))
+    print("  Interface config line: {0}".format(obj.text))
+    print("  Interface mode: {0}".format(intf_mode))
+    print("  IP address: {0}".format(ip_addr))
