04896778f44eabff9974131679c2126ba84c24bc
mpenning <mike@pennington.net>
2020-05-05 05:47:32 -0500

    Upgrade to fabric2 syntax

--- deploy_docs.py
+++ deploy_docs.py
@@ -1,29 +1,42 @@
-from fabric.api import run, local, cd, lcd, put, settings
-# See:
-#     http://docs.fabfile.org/en/latest/api/core/operations.html
+# Old fabric v1 methods:
+#from fabric.api import run, local, cd, lcd, put, settings
+from getpass import getpass
+import sys
+import os
+
+from fabric import Connection
+from invoke import run
 
 def deploy_ccp_docs(ccp_doc_root="public_html/py/ciscoconfparse",
     ccp_bundle_name="ccp.tar.gz", doc_host=""):
 
-    with lcd('sphinx-doc'):
-        local('make html')  # local command
+    # Run 'make html' in directory: sphinx-doc/
+    run('cd sphinx-doc && make html')  # local command
 
-    with lcd('sphinx-doc/_build/html'):
-        local('tar cvfz ~/{0} *'.format(ccp_bundle_name))
+    run('cd sphinx-doc/_build/html && tar cvfz {0} *'.format(
+        os.path.expanduser("~/"+ccp_bundle_name)))
 
-    with lcd('sphinx-doc'):
-        local('make clean')
+    # Run 'make clean' in directory: sphinx-doc/
+    run('cd sphinx-doc && make clean')  # local command
 
-    with settings(host_string=doc_host):
-        # scp file to server
-        put(local_path="~/{0}".format(ccp_bundle_name), 
-            remote_path=ccp_bundle_name)
-        with cd(ccp_doc_root):
-            run("rm -rf *")  # Remote command
-            run("mv ~/{0} .".format(ccp_bundle_name))
-            run("tar xvfz {0}".format(ccp_bundle_name))
-            run("rm {0}".format(ccp_bundle_name))
+    # ssh with a password...
+    conn = Connection('mpenning@{}'.format(doc_host),
+        connect_kwargs={"password": getpass()})
+    conn.put(local=os.path.expanduser("~/{0}".format(ccp_bundle_name)), 
+        remote=ccp_bundle_name)
+
+    # Delete all the old files
+    conn.run("rm -rf {0}/*".format(ccp_doc_root))
+    # Move the new files to ccp_doc_root
+    conn.run("mv {0} {1}".format(ccp_bundle_name, ccp_doc_root))
+
+    with conn.cd(ccp_doc_root):
+        conn.run("tar xvfz {0}".format(ccp_bundle_name))
+        conn.run("rm {0}".format(ccp_bundle_name))
 
 if __name__=="__main__":
-    doc_host = raw_input("Documentation host: ")
+    if sys.version_info>=(3, 0):
+        doc_host = input("Documentation host: ")
+    else:
+        doc_host = raw_input("Documentation host: ")
     deploy_ccp_docs(doc_host=doc_host.strip())
