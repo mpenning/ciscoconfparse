5c8040a2645e6182cbbb457d9edd0c8e2b458747
mpenning <mike@pennington.net>
2020-07-11 18:31:27 -0500

    Reformat with black

--- deploy_docs.py
+++ deploy_docs.py
@@ -1,5 +1,5 @@
 # Old fabric v1 methods:
-#from fabric.api import run, local, cd, lcd, put, settings
+# from fabric.api import run, local, cd, lcd, put, settings
 from getpass import getpass
 import sys
 import os
@@ -7,23 +7,34 @@ import os
 from fabric import Connection
 from invoke import run
 
-def deploy_ccp_docs(ccp_doc_root="public_html/py/ciscoconfparse",
-    ccp_bundle_name="ccp.tar.gz", doc_host="", password=""):
+
+def deploy_ccp_docs(
+    ccp_doc_root="public_html/py/ciscoconfparse",
+    ccp_bundle_name="ccp.tar.gz",
+    doc_host="",
+    password="",
+):
 
     # Run 'make html' in directory: sphinx-doc/
-    run('cd sphinx-doc && make clean && make html')  # local command
+    run("cd sphinx-doc && make clean && make html")  # local command
 
-    run('cd sphinx-doc/_build/html && tar cvfz {0} *'.format(
-        os.path.expanduser("~/"+ccp_bundle_name)))
+    run(
+        "cd sphinx-doc/_build/html && tar cvfz {0} *".format(
+            os.path.expanduser("~/" + ccp_bundle_name)
+        )
+    )
 
     # Run 'make clean' in directory: sphinx-doc/
-    run('cd sphinx-doc && make clean')  # local command
+    run("cd sphinx-doc && make clean")  # local command
 
     # ssh with a password...
-    conn = Connection('mpenning@{}'.format(doc_host),
-        connect_kwargs={"password": password})
-    conn.put(local=os.path.expanduser("~/{0}".format(ccp_bundle_name)), 
-        remote=ccp_bundle_name)
+    conn = Connection(
+        "mpenning@{}".format(doc_host), connect_kwargs={"password": password}
+    )
+    conn.put(
+        local=os.path.expanduser("~/{0}".format(ccp_bundle_name)),
+        remote=ccp_bundle_name,
+    )
 
     # Delete all the old files
     conn.run("rm -rf {0}/*".format(ccp_doc_root))
@@ -34,8 +45,9 @@ def deploy_ccp_docs(ccp_doc_root="public_html/py/ciscoconfparse",
         conn.run("tar xvfz {0}".format(ccp_bundle_name))
         conn.run("rm {0}".format(ccp_bundle_name))
 
-if __name__=="__main__":
-    if sys.version_info>=(3, 0):
+
+if __name__ == "__main__":
+    if sys.version_info >= (3, 0):
         doc_host = input("Documentation host: ")
     else:
         doc_host = raw_input("Documentation host: ")
