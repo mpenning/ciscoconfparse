728c76ce69a8f820c796fff3cb769c495c375329
mpenning <mike@pennington.net>
2021-10-07 18:33:44 -0500

    Update pytest CLI arguments

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -3,12 +3,14 @@
 set -eu
 
 # Change into your virtualenv before running the script
-py.test -s -x -l --show-capture=all --tb=long test_CiscoConfParse.py
+
+# --durations=5 triggers a report for the five slowest tests...
+py.test -sxl --durations=5 --show-capture=all --tb=long test_CiscoConfParse.py
 sleep 0.05
-py.test -s -x -l --show-capture=all --tb=long test_Ccp_Util.py
+py.test -sxl --durations=5 --show-capture=all --tb=long test_Ccp_Util.py
 sleep 0.05
-py.test -s -x -l --show-capture=all --tb=long test_Models_Cisco.py
+py.test -sxl --durations=5 --show-capture=all --tb=long test_Models_Cisco.py
 sleep 0.05
-py.test -s -x -l --show-capture=all --tb=long test_Models_Asa.py
+py.test -sxl --durations=5 --show-capture=all --tb=long test_Models_Asa.py
 sleep 0.05
-py.test -s -x -l --show-capture=all --tb=long test_Models_Junos.py
+py.test -sxl --durations=5 --show-capture=all --tb=long test_Models_Junos.py
