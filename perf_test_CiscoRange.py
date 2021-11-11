0f2ecf1b73214fad582cf2e432b94febb8c5ef99
mpenning <mike@pennington.net>
2021-11-11 16:17:43 -0600

    Rename and relocate

--- perf_test_CiscoRange.py
+++
@@ -1,8 +0,0 @@
-from timeit import timeit
-
-vlan_text = "1-1000,1001-2000,2001-3000,3001-4000,4001-4094"
-time_CiscoRange_all_vlans_int = timeit(
-    'CiscoRange(text="%s", result_type=int)' % vlan_text,
-    setup='from ciscoconfparse.ccp_util import CiscoRange',
-    number=1000)
-print(time_CiscoRange_all_vlans_int)
