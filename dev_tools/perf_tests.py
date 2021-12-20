de028d1d3e9233cf9718659435c54bcf8aaf7b3b
mpenning <mike@pennington.net>
2021-12-20 07:55:57 -0600

    Increase timeit() call readability

--- dev_tools/perf_tests.py
+++ dev_tools/perf_tests.py
@@ -3,39 +3,38 @@ import sys
 import re
 import os
 
-#sys.path.insert(0, "../../")
-#sys.path.insert(0, "../")
-#venv_path = os.environ["VIRTUAL_ENV"]
-#python_path = os.environ["PYTHONPATH"]
-
 iterations = 1000
 
-"""
-timeit(stmt='pass', setup='pass', timer=<default timer>, number=1000000, globals=None)¶
-"""
-list_getattr = timeit(
-    stmt="bool(getattr(list(), 'append'))",
-    setup="pass",
-    number=10000
-)
-print("GETATTR append", list_getattr)
+CONFIG_FILEPATH = None
+# Set CONFIG_FILEPATH as a relative path
+for base_dir, dirs, files in os.walk("./"):
+    for filename in files:
+        if filename=="sample_01.ios":
+            CONFIG_FILEPATH = '%s/%s' % (base_dir, filename)
+            break
 
+if CONFIG_FILEPATH is None:
+    raise RuntimeError('RuntimeError - Could not find a config to parse')
+
+# Regarding how to import a variable into timeit(), 
+#     (for example: 'from __main__ import CONFIG_FILEPATH'),
+#     see https://stackoverflow.com/a/18882784/667301
 list_cast = timeit(
     stmt="list(map(attrgetter('text'), parse.ConfigObjs))",
-    setup="from ciscoconfparse import CiscoConfParse;from operator import methodcaller, attrgetter;parse=CiscoConfParse(config='configs/sample_01.ios')",
-    number=10000,
+    setup="from __main__ import CONFIG_FILEPATH;from ciscoconfparse import CiscoConfParse;from operator import methodcaller, attrgetter;parse=CiscoConfParse(config=CONFIG_FILEPATH)",
+    number=iterations,
 )
 
 list_comprehension = timeit(
     stmt="[ii.text for ii in parse.ConfigObjs]",
-    setup="from ciscoconfparse import CiscoConfParse;from operator import methodcaller, attrgetter;parse=CiscoConfParse(config='../configs/sample_01.ios')",
-    number=10000,
+    setup="from __main__ import CONFIG_FILEPATH;from ciscoconfparse import CiscoConfParse;from operator import methodcaller, attrgetter;parse=CiscoConfParse(config=CONFIG_FILEPATH)",
+    number=iterations,
 )
 
 list_comprehension_getattr = timeit(
     "[getattr(ii, 'text') for ii in parse.ConfigObjs]",
-    setup="from ciscoconfparse import CiscoConfParse;from operator import methodcaller, attrgetter;parse=CiscoConfParse(config='../configs/sample_01.ios')",
-    number=10000,
+    setup="from __main__ import CONFIG_FILEPATH;from ciscoconfparse import CiscoConfParse;from operator import methodcaller, attrgetter;parse=CiscoConfParse(config=CONFIG_FILEPATH)",
+    number=iterations,
 )
 
 print("CAST TO LIST", list_cast)
@@ -68,6 +67,6 @@ result_standard = timeit(
     #'rr = re.compile(r"(\w?)(?P<numbers>\d+)(\S+)");ss=rr.search("");ss.groups();ss.groupdict()',
     'ss = rr.search("FoMike12325234234nananana");ss.groups();ss.groupdict()',
     setup='import re;rr = re.compile(r"(\w?)(?P<numbers3>\d+)(\S+)")',
-    number=1000,
+    number=iterations,
 )
 print("STANDARD REGEX", result_standard)
