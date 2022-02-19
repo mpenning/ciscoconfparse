65f1a0de7e99e3b0b5802041770585686d16b1e4
mpenning <mike@pennington.net>
2022-02-19 16:00:17 -0600

    Improve documentation

--- dev_tools/compare_v4_v6_methods.py
+++ dev_tools/compare_v4_v6_methods.py
@@ -1,4 +1,6 @@
 
+"""Compare methods on IPv4Obj() and IPv6Obj().  Flag missing methods"""
+
 import sys
 import os
 sys.path.insert(0, "../")   # add the path to the local git repo copy
@@ -6,16 +8,20 @@ sys.path.insert(0, "../")   # add the path to the local git repo copy
 environ = os.environ['VIRTUAL_ENV']
 print("ENV", environ)
 
-print(str(os.environ['PYTHONPATH']))
 from ciscoconfparse import IPv4Obj, IPv6Obj
+
+try:
+    print("PYTHONPATH", str(os.environ['PYTHONPATH']))
+except:
+    pass
 
 v4_list = dir(IPv4Obj("127.0.0.1"))
 v6_list = dir(IPv6Obj("::1"))
 
-for ii_v4 in v4:
+for ii_v4 in v4_list:
     if not (ii_v4 in v6_list):
         print("IPv6Obj() is missing method:", ii_v4)
 
-for ii_v6 in v6:
+for ii_v6 in v6_list:
     if not (ii_v6 in v4_list):
         print("IPv4Obj() is missing method:", ii_v6)
