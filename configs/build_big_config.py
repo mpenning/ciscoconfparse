b04d996797a6ae517a319731deeb4f577b9a2c0e
mpenning <mike@pennington.net>
2022-04-16 04:51:29 -0500

    Upgrade to python3 syntax

--- configs/build_big_config.py
+++ configs/build_big_config.py
@@ -2,49 +2,49 @@ from random import randint, choice
 import sys
 
 if sys.argv[1]=="1":
-    print "!"
-    print "!"
+    print("!")
+    print("!")
     for vlan in range(1, 4095):
-        print "vlan {0}".format(vlan)
-        print " name VLAN_{0}".format(vlan)
-        print "!"
-    print "!"
+        print("vlan {}".format(vlan))
+        print(" name VLAN_{}".format(vlan))
+        print("!")
+    print("!")
     for slot in range(1, 14):
         for port in range(1, 49):
-            print "interface GigabitEthernet {0}/{1}".format(slot, port)
+            print("interface GigabitEthernet {}/{}".format(slot, port))
             mode = choice(['trunk', 'access'])
             if mode=='trunk':
-                print " switchport"
-                print " switchport trunk encapsulation dot1q"
-                print " switchport mode trunk"
-                print " switchport trunk allowed vlan 1-{0}".format(randint(2, 4094))
-                print " switchport nonnegotiate"
-                print " spanning-tree guard root"
-                print "!"
+                print(" switchport")
+                print(" switchport trunk encapsulation dot1q")
+                print(" switchport mode trunk")
+                print(" switchport trunk allowed vlan 1-{}".format(randint(2, 4094)))
+                print(" switchport nonnegotiate")
+                print(" spanning-tree guard root")
+                print("!")
             elif mode=='access':
-                print " switchport"
-                print " switchport access vlan {0}".format(randint(2, 4094))
-                print " switchport mode access"
-                print " switchport nonnegotiate"
-                print " spanning-tree portfast"
-                print "!"
+                print(" switchport")
+                print(" switchport access vlan {}".format(randint(2, 4094)))
+                print(" switchport mode access")
+                print(" switchport nonnegotiate")
+                print(" spanning-tree portfast")
+                print("!")
     for vlan in range(1, 4095):
-        print "interface Vlan {0}".format(vlan)
-        print " no shutdown"
+        print("interface Vlan {}".format(vlan))
+        print(" no shutdown")
         mode = choice(['global', 'vrf'])
         if mode=='vrf':
-            print "ip vrf forwarding VRF_{0}".format(vlan)
-        print " description Layer3 SVI: vlan {0}".format(vlan)
-        print " ip address {0}.{1}.{2}.0 255.255.255.0".format(randint(1, 224),
-            randint(1, 255), randint(1, 255))
-        print "!"
+            print("ip vrf forwarding VRF_{}".format(vlan))
+        print(" description Layer3 SVI: vlan {}".format(vlan))
+        print(" ip address {}.{}.{}.0 255.255.255.0".format(randint(1, 224),
+            randint(1, 255), randint(1, 255)))
+        print("!")
 
 elif sys.argv[1]=="5":
-    print "!"
-    print "!"
-    print "!"
+    print("!")
+    print("!")
+    print("!")
     for acl_number in range(0, 100):
-        print "ip access-list extended ACL_{0}".format('%02i' % acl_number)
+        print("ip access-list extended ACL_{}".format('%02i' % acl_number))
         for ace in range(0, randint(20, 254)):
-            print " permit {0} any host 192.0.2.{1}".format(choice(['tcp', 'udp', 'ip']), ace)
-        print "!"
+            print(" permit {} any host 192.0.2.{}".format(choice(['tcp', 'udp', 'ip']), ace))
+        print("!")
