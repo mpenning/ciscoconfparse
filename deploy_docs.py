b9abc273844645ecc7860ce5d6aa5336edc9efb5
mpenning <mike@pennington.net>
2022-04-24 14:54:52 -0500

    Add docstring

--- deploy_docs.py
+++ deploy_docs.py
@@ -14,12 +14,15 @@ def deploy_ccp_docs(
     doc_host="",
     password="",
 ):
+    """
+    Build and push ciscoconfparse documentation to public webhost
+    """
 
     # Run 'make html' in directory: sphinx-doc/
     run("cd sphinx-doc && make clean && make html")  # local command
 
     run(
-        "cd sphinx-doc/_build/html && tar cvfz {0} *".format(
+        "cd sphinx-doc/_build/html && tar cvfz {} *".format(
             os.path.expanduser("~/" + ccp_bundle_name)
         )
     )
@@ -32,24 +35,21 @@ def deploy_ccp_docs(
         "mpenning@{}".format(doc_host), connect_kwargs={"password": password}
     )
     conn.put(
-        local=os.path.expanduser("~/{0}".format(ccp_bundle_name)),
+        local=os.path.expanduser("~/{}".format(ccp_bundle_name)),
         remote=ccp_bundle_name,
     )
 
     # Delete all the old files
-    conn.run("rm -rf {0}/*".format(ccp_doc_root))
+    conn.run("rm -rf {}/*".format(ccp_doc_root))
     # Move the new files to ccp_doc_root
-    conn.run("mv {0} {1}".format(ccp_bundle_name, ccp_doc_root))
+    conn.run("mv {} {}".format(ccp_bundle_name, ccp_doc_root))
 
     with conn.cd(ccp_doc_root):
-        conn.run("tar xvfz {0}".format(ccp_bundle_name))
-        conn.run("rm {0}".format(ccp_bundle_name))
+        conn.run("tar xvfz {}".format(ccp_bundle_name))
+        conn.run("rm {}".format(ccp_bundle_name))
 
 
 if __name__ == "__main__":
-    if sys.version_info >= (3, 0):
-        doc_host = input("Documentation host: ")
-    else:
-        doc_host = raw_input("Documentation host: ")
+    doc_host = input("Documentation host: ")
     password = getpass()
     deploy_ccp_docs(doc_host=doc_host.strip(), password=password)
