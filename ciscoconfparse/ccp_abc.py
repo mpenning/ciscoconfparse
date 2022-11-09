9759fdbb756f5c82d5beb3aa75619ba6d3f7a549
mpenning <mike@pennington.net>
2022-11-09 05:00:30 -0600

    Update copyrights

--- ciscoconfparse/ccp_abc.py
+++ ciscoconfparse/ccp_abc.py
@@ -1,5 +1,6 @@
 r""" ccp_abc.py - Parse, Query, Build, and Modify IOS-style configurations
-     Copyright (C) 2021-2022 David Michael Pennington
+     Copyright (C) 2022 David Michael Pennington
+     Copyright (C) 2022 David Michael Pennington at WellSky
      Copyright (C) 2020-2021 David Michael Pennington at Cisco Systems
      Copyright (C) 2019      David Michael Pennington at ThousandEyes
      Copyright (C) 2014-2019 David Michael Pennington at Samsung Data Services
--- ciscoconfparse/ciscoconfparse.py
+++ ciscoconfparse/ciscoconfparse.py
@@ -1,6 +1,7 @@
 """ ciscoconfparse.py - Parse, Query, Build, and Modify IOS-style configs
 
-     Copyright (C) 2021-2022 David Michael Pennington
+     Copyright (C) 2022 David Michael Pennington
+     Copyright (C) 2022 David Michael Pennington at WellSky
      Copyright (C) 2019-2021 David Michael Pennington at Cisco Systems / ThousandEyes
      Copyright (C) 2012-2019 David Michael Pennington at Samsung Data Services
      Copyright (C) 2011-2012 David Michael Pennington at Dell Computer Corp
