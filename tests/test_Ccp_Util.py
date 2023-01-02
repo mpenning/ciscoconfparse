1fe662f7034bf94418803134a0615f433bfe1a00
mpenning <mike@pennington.net>
2023-01-02 09:08:44 -0600

    Fix up cosmetic formatting problems

--- tests/test_Ccp_Util.py
+++ tests/test_Ccp_Util.py
@@ -44,7 +44,7 @@ else:
     from ipaddress import IPv4Network, IPv6Network, IPv4Address, IPv6Address
     import ipaddress
 
-import dns.resolver
+from dns.resolver import Timeout
 
 
 @pytest.mark.parametrize(
@@ -135,10 +135,12 @@ def testL4Object_asa_lt01():
     assert pp.protocol == "tcp"
     assert pp.port_list ==sorted(range(1, 7))
 
+
 def testL4Object_asa_gt01():
     pp = L4Object(protocol="tcp", port_spec="gt 65534", syntax="asa")
     assert pp.protocol == "tcp"
     assert pp.port_list ==[65535]
+
 
 @pytest.mark.xfail(
     sys.version_info[0] == 3 and sys.version_info[1] == 2,
@@ -165,6 +167,7 @@ def testIPv4Obj_contains_01():
         ## 'foo in bar' tests bar.__contains__(foo)
         test_result = IPv4Obj(prefix1) in IPv4Obj(prefix2)
         assert test_result == result_correct
+
 
 def testIPv4Obj_contains_02():
 
@@ -200,6 +203,7 @@ def testIPv4Obj_parse(addr_mask):
     test_result = IPv4Obj(addr_mask)
     assert isinstance(test_result, IPv4Obj)
 
+
 def testIPv4Obj_set_masklen01():
 
     MASK_RESET = 32
@@ -226,6 +230,7 @@ def testIPv4Obj_set_masklen01():
         assert test_object.masklength == result_correct_masklen
         assert test_object.prefixlen == result_correct_masklen
         assert test_object.prefixlength == result_correct_masklen
+
 
 def testIPv4Obj_attributes_01():
     ## Ensure that attributes are accessible and pass the smell test
@@ -260,76 +265,79 @@ def testIPv4Obj_attributes_01():
 
         assert getattr(test_object, attribute) == result_correct
 
+
 def test_ip_factory_inputs_01():
     """Test input / output of ccp_util.ip_factory()"""
     # Test whether IPv4Obj is retured
     test_params = (
-            # Test format...
-            #    (<dict with test inputs>, result_correct)
-            ({'val': '1.1.1.1/16', 'stdlib': False}, IPv4Obj("1.1.1.1/16")),
-            ({'val': '1.1.1.1/16', 'stdlib': True},  IPv4Network("1.1.1.1/16", strict=False)),
-            ({'val': '1.1.1.1/32', 'stdlib': False}, IPv4Obj("1.1.1.1/32")),
-            ({'val': '1.1.1.1/32', 'stdlib': True},  IPv4Address("1.1.1.1")),
-            ({'val': '2b00:cd80:14:10::1/64', 'stdlib': False}, IPv6Obj("2b00:cd80:14:10::1/64")),
-            ({'val': '2b00:cd80:14:10::1/64', 'stdlib': True}, IPv6Network("2b00:cd80:14:10::/64", strict=True)),
-            ({'val': '::1/64', 'stdlib': False}, IPv6Obj("::1/64")),
-            ({'val': '::1/64', 'stdlib': True},  IPv6Network("::0/64")),
-            ({'val': '::1/128', 'stdlib': False}, IPv6Obj("::1/128")),
-            ({'val': '::1/128', 'stdlib': True},  IPv6Address("::1")),
+        # Test format...
+        #    (<dict with test inputs>, result_correct)
+        ({'val': '1.1.1.1/16', 'stdlib': False}, IPv4Obj("1.1.1.1/16")),
+        ({'val': '1.1.1.1/16', 'stdlib': True},  IPv4Network("1.1.1.1/16", strict=False)),
+        ({'val': '1.1.1.1/32', 'stdlib': False}, IPv4Obj("1.1.1.1/32")),
+        ({'val': '1.1.1.1/32', 'stdlib': True},  IPv4Address("1.1.1.1")),
+        ({'val': '2b00:cd80:14:10::1/64', 'stdlib': False}, IPv6Obj("2b00:cd80:14:10::1/64")),
+        ({'val': '2b00:cd80:14:10::1/64', 'stdlib': True}, IPv6Network("2b00:cd80:14:10::/64", strict=True)),
+        ({'val': '::1/64', 'stdlib': False}, IPv6Obj("::1/64")),
+        ({'val': '::1/64', 'stdlib': True},  IPv6Network("::0/64")),
+        ({'val': '::1/128', 'stdlib': False}, IPv6Obj("::1/128")),
+        ({'val': '::1/128', 'stdlib': True},  IPv6Address("::1")),
         )
     for test_args, result_correct in test_params:
         assert ip_factory(**test_args)==result_correct
+
 
 def test_ip_factory_inputs_02():
     """Test input / output of ccp_util.ip_factory().  This tests checks error conditions"""
 
     # invalid ip address...
-    with pytest.raises(ipaddress.AddressValueError) as ee:
+    with pytest.raises(ipaddress.AddressValueError):
         ip_factory("brickTrick", stdlib=False, mode="auto_detect")
 
     # invalid ipv4 address
-    with pytest.raises(ipaddress.AddressValueError) as ee:
+    with pytest.raises(ipaddress.AddressValueError):
         ip_factory("brickTrick", stdlib=False, mode="ipv4")
 
     # invalid ipv6 address
-    with pytest.raises(ipaddress.AddressValueError) as ee:
+    with pytest.raises(ipaddress.AddressValueError):
         ip_factory("brickTrick", stdlib=False, mode="ipv6")
 
     # invalid ipv4 address
-    with pytest.raises(ipaddress.AddressValueError) as ee:
+    with pytest.raises(ipaddress.AddressValueError):
         ip_factory("::1/128", stdlib=False, mode="ipv4")
 
     # invalid ipv4 address
-    with pytest.raises(ipaddress.AddressValueError) as ee:
+    with pytest.raises(ipaddress.AddressValueError):
         ip_factory("1.2.3.4.5", stdlib=False, mode="ipv4")
 
     # invalid first octet and parse auto_detect...
-    with pytest.raises(ipaddress.AddressValueError) as ee:
+    with pytest.raises(ipaddress.AddressValueError):
         ip_factory("256.1.1.1", stdlib=False, mode="auto_detect")
 
     # invalid first octet and parse as ipv4
-    with pytest.raises(ipaddress.AddressValueError) as ee:
+    with pytest.raises(ipaddress.AddressValueError):
         ip_factory("256.1.1.1", stdlib=False, mode="ipv4")
 
     # invalid first octet and parse as ipv6
-    with pytest.raises(ipaddress.AddressValueError) as ee:
+    with pytest.raises(ipaddress.AddressValueError):
         ip_factory("256.1.1.1", stdlib=False, mode="ipv6")
 
     # netmask too long...
-    with pytest.raises(ipaddress.AddressValueError) as ee:
+    with pytest.raises(ipaddress.AddressValueError):
         ip_factory("239.1.1.1/33", stdlib=False, mode="ipv4")
 
     # parse ipv6 as ipv6...
-    with pytest.raises(ipaddress.AddressValueError) as ee:
+    with pytest.raises(ipaddress.AddressValueError):
         ip_factory("239.1.1.1/24", stdlib=False, mode="ipv6")
 
     # parse invalid ipv6 address...
-    with pytest.raises(ipaddress.AddressValueError) as ee:
+    with pytest.raises(ipaddress.AddressValueError):
         ip_factory("FE80:AAAA::DEAD:BEEEEEEEEEEF", stdlib=False, mode="ipv6")
 
     # parse invalid auto_detect ipv6 address...
-    with pytest.raises(ipaddress.AddressValueError) as ee:
+    with pytest.raises(ipaddress.AddressValueError):
         ip_factory("FE80:AAAA::DEAD:BEEEEEEEEEEF", stdlib=False, mode="auto_detect")
+
 
 def testIPv6Obj_attributes():
     ## Ensure that attributes are accessible and pass the smell test
@@ -445,8 +453,10 @@ def testIPv4Obj_recursive():
     assert str(obj.ip_object) == "1.1.1.1"
     assert obj.prefixlen == 24
 
+
 def testIPv4Obj_from_int():
     assert IPv4Obj(2886729984).ip == IPv4Address('172.16.1.0')
+
 
 def testIPv4Obj_neq_01():
     """Simple in-equality test fail (ref - Github issue #180)"""
@@ -557,9 +567,11 @@ def testIPv6Obj_gt_01():
     """Simple greater_than test"""
     assert IPv6Obj("::2") > IPv6Obj("::1")
 
+
 def testIPv6Obj_lt_01():
     """Simple less_than test"""
     assert IPv6Obj("::1") < IPv6Obj("::2")
+
 
 def test_collapse_addresses_01():
 
@@ -569,6 +581,7 @@ def test_collapse_addresses_01():
     for idx, entry in enumerate(net_collapsed):
         if idx==0:
             assert entry == IPv4Network("192.0.0.0/22")
+
 
 def test_collapse_addresses_02():
     net_list = [IPv4Obj('192.0.2.128/25'), IPv4Obj('192.0.0.0/26')]
@@ -584,7 +597,7 @@ def test_dns_lookup():
     result_correct = {"addrs": [result_correct_address], "name": test_hostname, "error": "", "record_type": "A"}
     try:
         test_result = dns_lookup(test_hostname)
-    except dns.resolver.LifetimeTimeout:
+    except Timeout:
         pytest.skip("Skipping due to DNS resolver timeout")
 
     if test_result["error"] != "":
@@ -598,11 +611,12 @@ def test_reverse_dns_lookup():
     result_correct = {"addrs": ["127.0.0.1"], "name": "localhost.", "error": ""}
 
     test_result = reverse_dns_lookup("127.0.0.1")
+    assert isinstance(test_result, dict)
     assert test_result["name"] != ""
     assert "localhost" in test_result["name"].lower()
     try:
         assert test_result["error"] == ""
-    except:
+    except Exception:
         pytest.skip(test_result["error"])
 
 
@@ -625,64 +639,52 @@ def test_CiscoRange_03():
     assert CiscoRange("1,2-4,5").as_list == result_correct
 
 
-def test_CiscoRange_03():
+def test_CiscoRange_04():
     """Basic vlan range test"""
     result_correct = ["1", "2", "3", "4", "5"]
     assert CiscoRange("1-3,4,5").as_list == result_correct
 
 
-def test_CiscoRange_04():
+def test_CiscoRange_05():
     """Basic vlan range test"""
     result_correct = ["1", "2", "3", "4", "5"]
     assert CiscoRange("1,2,3-5").as_list == result_correct
 
 
-def test_CiscoRange_05():
+def test_CiscoRange_06():
     """Basic slot range test"""
     result_correct = ["1/1", "1/2", "1/3", "1/4", "1/5"]
     assert CiscoRange("1/1-3,4,5").as_list == result_correct
 
 
-def test_CiscoRange_06():
+def test_CiscoRange_07():
     """Basic slot range test"""
     result_correct = ["1/1", "1/2", "1/3", "1/4", "1/5"]
     assert CiscoRange("1/1,2-4,5").as_list == result_correct
 
 
-def test_CiscoRange_07():
+def test_CiscoRange_08():
     """Basic slot range test"""
     result_correct = ["1/1", "1/2", "1/3", "1/4", "1/5"]
     assert CiscoRange("1/1,2,3-5").as_list == result_correct
 
 
-def test_CiscoRange_08():
+def test_CiscoRange_09():
     """Basic slot range test"""
     result_correct = ["2/1/1", "2/1/2", "2/1/3", "2/1/4", "2/1/5"]
     assert CiscoRange("2/1/1-3,4,5").as_list == result_correct
 
 
-def test_CiscoRange_09():
+def test_CiscoRange_10():
     """Basic slot range test"""
     result_correct = ["2/1/1", "2/1/2", "2/1/3", "2/1/4", "2/1/5"]
     assert CiscoRange("2/1/1,2-4,5").as_list == result_correct
 
 
-def test_CiscoRange_10():
+def test_CiscoRange_11():
     """Basic slot range test"""
     result_correct = ["2/1/1", "2/1/2", "2/1/3", "2/1/4", "2/1/5"]
     assert CiscoRange("2/1/1,2,3-5").as_list == result_correct
-
-
-def test_CiscoRange_11():
-    """Basic interface slot range test"""
-    result_correct = [
-        "interface Eth2/1/1",
-        "interface Eth2/1/2",
-        "interface Eth2/1/3",
-        "interface Eth2/1/4",
-        "interface Eth2/1/5",
-    ]
-    assert CiscoRange("interface Eth2/1/1-3,4,5").as_list == result_correct
 
 
 def test_CiscoRange_12():
@@ -694,7 +696,7 @@ def test_CiscoRange_12():
         "interface Eth2/1/4",
         "interface Eth2/1/5",
     ]
-    assert CiscoRange("interface Eth2/1/1,2-4,5").as_list == result_correct
+    assert CiscoRange("interface Eth2/1/1-3,4,5").as_list == result_correct
 
 
 def test_CiscoRange_13():
@@ -706,10 +708,22 @@ def test_CiscoRange_13():
         "interface Eth2/1/4",
         "interface Eth2/1/5",
     ]
-    assert CiscoRange("interface Eth2/1/1,2,3-5").as_list == result_correct
+    assert CiscoRange("interface Eth2/1/1,2-4,5").as_list == result_correct
 
 
 def test_CiscoRange_14():
+    """Basic interface slot range test"""
+    result_correct = [
+        "interface Eth2/1/1",
+        "interface Eth2/1/2",
+        "interface Eth2/1/3",
+        "interface Eth2/1/4",
+        "interface Eth2/1/5",
+    ]
+    assert CiscoRange("interface Eth2/1/1,2,3-5").as_list == result_correct
+
+
+def test_CiscoRange_15():
     """Basic interface slot range test"""
     result_correct = [
         "interface Eth 2/1/1",
@@ -721,18 +735,19 @@ def test_CiscoRange_14():
     assert CiscoRange("interface Eth 2/1/1,2,3-5").as_list == result_correct
 
 
-def test_CiscoRange_15():
+def test_CiscoRange_16():
     """Empty range test"""
     result_correct = []
     assert CiscoRange("").as_list == result_correct
 
 
-def test_CiscoRange_16():
+def test_CiscoRange_17():
     """Append range test"""
     result_correct = [1, 2, 3]
     assert CiscoRange("", result_type=int).append("1-3").as_list == result_correct
 
-def test_CiscoRange_17():
+
+def test_CiscoRange_18():
     """Parse a string with a common prefix on all of the CiscoRange() inputs"""
     result_correct = [
         "Eth1/1",
@@ -741,7 +756,8 @@ def test_CiscoRange_17():
     ]
     CiscoRange("Eth1/1,Eth1/12-20,Eth1/16,Eth1/10").as_list == result_correct
 
-def test_CiscoRange_18():
+
+def test_CiscoRange_19():
     """Parse a string with a common prefix on all of the CiscoRange() inputs"""
     result_correct = [
         "interface Eth1/1",
