f295c9586718988f07cae5da548fd2ef0e94a6e5
mpenning <mike@pennington.net>
2023-05-12 05:56:50 -0500

    Add import in conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -27,6 +27,7 @@ import os
 sys.path.insert(0, "..")
 
 from ciscoconfparse.ciscoconfparse import CiscoConfParse
+from ciscoconfparse.ccp_util import PythonOptimizeCheck
 import pytest
 
 THIS_DIR = os.path.dirname(__file__)
