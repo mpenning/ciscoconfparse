db0dc944046cede01ff0a7a13c7297096ad941c4
mpenning <mike@pennington.net>
2023-10-10 16:22:02 -0500

    Silence random cryptographic warnings

--- tests/fixtures/configs/build_big_config.py
+++ tests/fixtures/configs/build_big_config.py
@@ -17,13 +17,15 @@ if sys.argv[1]=="1":
                 print(" switchport")
                 print(" switchport trunk encapsulation dot1q")
                 print(" switchport mode trunk")
-                print(" switchport trunk allowed vlan 1-{}".format(randint(2, 4094)))
+                # nosec will silence warnings about python random cryptographic inadequacy
+                print(" switchport trunk allowed vlan 1-{}".format(randint(2, 4094))) # nosec
                 print(" switchport nonnegotiate")
                 print(" spanning-tree guard root")
                 print("!")
             elif mode=='access':
                 print(" switchport")
-                print(" switchport access vlan {}".format(randint(2, 4094)))
+                # nosec will silence warnings about python random cryptographic inadequacy
+                print(" switchport access vlan {}".format(randint(2, 4094))) # nosec
                 print(" switchport mode access")
                 print(" switchport nonnegotiate")
                 print(" spanning-tree portfast")
@@ -35,8 +37,9 @@ if sys.argv[1]=="1":
         if mode=='vrf':
             print("ip vrf forwarding VRF_{}".format(vlan))
         print(" description Layer3 SVI: vlan {}".format(vlan))
-        print(" ip address {}.{}.{}.0 255.255.255.0".format(randint(1, 224),
-            randint(1, 255), randint(1, 255)))
+        print(" ip address {}.{}.{}.0 255.255.255.0".format(
+            randint(1, 224),                   # nosec
+            randint(1, 255), randint(1, 255))) # nosec
         print("!")
 
 elif sys.argv[1]=="5":
@@ -45,6 +48,11 @@ elif sys.argv[1]=="5":
     print("!")
     for acl_number in range(0, 100):
         print("ip access-list extended ACL_{}".format('%02i' % acl_number))
-        for ace in range(0, randint(20, 254)):
-            print(" permit {} any host 192.0.2.{}".format(choice(['tcp', 'udp', 'ip']), ace))
+        # nosec will silence warnings about python random cryptographic inadequacy
+        for ace in range(0, randint(20, 254)): # nosec
+            print(" permit {} any host 192.0.2.{}".format(
+                # nosec will silence warnings about python random cryptographic inadequacy
+                choice(['tcp', 'udp', 'ip']), ace  # nosec
+                )
+            )
         print("!")
