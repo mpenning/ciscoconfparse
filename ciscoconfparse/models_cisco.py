b84c41a2a84c9262198f94b1ace31615f4a2c751
mpenning <mike@pennington.net>
2023-10-10 16:41:02 -0500

    Remove better_exceptions and traceback

--- ciscoconfparse/models_cisco.py
+++ ciscoconfparse/models_cisco.py
@@ -1,12 +1,5 @@
 from __future__ import absolute_import
-import traceback
 import re
-
-if False:
-    import better_exceptions
-    better_exceptions.MAX_LENGTH = None
-    better_exceptions.SUPPORTS_COLOR = True
-    better_exceptions.hook()
 
 from ciscoconfparse.errors import DynamicAddressException
 
