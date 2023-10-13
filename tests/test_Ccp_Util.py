ab68cb54c9e769d56ab6324ec73b0b3ca933698b
mpenning <mike@pennington.net>
2023-10-13 04:51:36 -0500

    Convert tests to use

--- tests/test_Ccp_Util.py
+++ tests/test_Ccp_Util.py
@@ -635,148 +635,148 @@ def test_reverse_dns_lookup():
 
 def test_CiscoRange_01():
     """Basic vlan range test"""
-    result_correct = ["1"]
-    assert CiscoRange("1").as_list == result_correct
+    result_correct = set({"1",})
+    assert CiscoRange("1").as_set == result_correct
 
 
 def test_CiscoRange_02():
     """Basic vlan range test"""
-    result_correct = ["1", "3"]
-    assert CiscoRange("1,3").as_list == result_correct
+    result_correct = set({"1", "3",})
+    assert CiscoRange("1,3").as_set == result_correct
 
 
 def test_CiscoRange_03():
     """Basic vlan range test"""
-    result_correct = ["1", "2", "3", "4", "5"]
-    assert CiscoRange("1,2-4,5").as_list == result_correct
+    result_correct = set({"1", "2", "3", "4", "5",})
+    assert CiscoRange("1,2-4,5").as_set == result_correct
 
 
 def test_CiscoRange_04():
     """Basic vlan range test"""
-    result_correct = ["1", "2", "3", "4", "5"]
-    assert CiscoRange("1-3,4,5").as_list == result_correct
+    result_correct = set({"1", "2", "3", "4", "5",})
+    assert CiscoRange("1-3,4,5").as_set == result_correct
 
 
 def test_CiscoRange_05():
     """Basic vlan range test"""
-    result_correct = ["1", "2", "3", "4", "5"]
-    assert CiscoRange("1,2,3-5").as_list == result_correct
+    result_correct = set({"1", "2", "3", "4", "5",})
+    assert CiscoRange("1,2,3-5").as_set == result_correct
 
 
 def test_CiscoRange_06():
     """Basic slot range test"""
-    result_correct = ["1/1", "1/2", "1/3", "1/4", "1/5"]
-    assert CiscoRange("1/1-3,4,5").as_list == result_correct
+    result_correct = set({"1/1", "1/2", "1/3", "1/4", "1/5",})
+    assert CiscoRange("1/1-3,4,5").as_set == result_correct
 
 
 def test_CiscoRange_07():
     """Basic slot range test"""
-    result_correct = ["1/1", "1/2", "1/3", "1/4", "1/5"]
-    assert CiscoRange("1/1,2-4,5").as_list == result_correct
+    result_correct = {"1/1", "1/2", "1/3", "1/4", "1/5"}
+    assert CiscoRange("1/1,2-4,5").as_set == result_correct
 
 
 def test_CiscoRange_08():
     """Basic slot range test"""
-    result_correct = ["1/1", "1/2", "1/3", "1/4", "1/5"]
-    assert CiscoRange("1/1,2,3-5").as_list == result_correct
+    result_correct = {"1/1", "1/2", "1/3", "1/4", "1/5"}
+    assert CiscoRange("1/1,2,3-5").as_set == result_correct
 
 
 def test_CiscoRange_09():
     """Basic slot range test"""
-    result_correct = ["2/1/1", "2/1/2", "2/1/3", "2/1/4", "2/1/5"]
-    assert CiscoRange("2/1/1-3,4,5").as_list == result_correct
+    result_correct = {"2/1/1", "2/1/2", "2/1/3", "2/1/4", "2/1/5"}
+    assert CiscoRange("2/1/1-3,4,5").as_set == result_correct
 
 
 def test_CiscoRange_10():
     """Basic slot range test"""
-    result_correct = ["2/1/1", "2/1/2", "2/1/3", "2/1/4", "2/1/5"]
-    assert CiscoRange("2/1/1,2-4,5").as_list == result_correct
+    result_correct = {"2/1/1", "2/1/2", "2/1/3", "2/1/4", "2/1/5"}
+    assert CiscoRange("2/1/1,2-4,5").as_set == result_correct
 
 
 def test_CiscoRange_11():
     """Basic slot range test"""
-    result_correct = ["2/1/1", "2/1/2", "2/1/3", "2/1/4", "2/1/5"]
-    assert CiscoRange("2/1/1,2,3-5").as_list == result_correct
+    result_correct = {"2/1/1", "2/1/2", "2/1/3", "2/1/4", "2/1/5"}
+    assert CiscoRange("2/1/1,2,3-5").as_set == result_correct
 
 
 def test_CiscoRange_12():
     """Basic interface slot range test"""
-    result_correct = [
+    result_correct = {
         "interface Eth2/1/1",
         "interface Eth2/1/2",
         "interface Eth2/1/3",
         "interface Eth2/1/4",
         "interface Eth2/1/5",
-    ]
-    assert CiscoRange("interface Eth2/1/1-3,4,5").as_list == result_correct
+    }
+    assert CiscoRange("interface Eth2/1/1-3,4,5").as_set == result_correct
 
 
 def test_CiscoRange_13():
     """Basic interface slot range test"""
-    result_correct = [
+    result_correct = {
         "interface Eth2/1/1",
         "interface Eth2/1/2",
         "interface Eth2/1/3",
         "interface Eth2/1/4",
         "interface Eth2/1/5",
-    ]
-    assert CiscoRange("interface Eth2/1/1,2-4,5").as_list == result_correct
+    }
+    assert CiscoRange("interface Eth2/1/1,2-4,5").as_set == result_correct
 
 
 def test_CiscoRange_14():
     """Basic interface slot range test"""
-    result_correct = [
+    result_correct = {
         "interface Eth2/1/1",
         "interface Eth2/1/2",
         "interface Eth2/1/3",
         "interface Eth2/1/4",
         "interface Eth2/1/5",
-    ]
-    assert CiscoRange("interface Eth2/1/1,2,3-5").as_list == result_correct
+    }
+    assert CiscoRange("interface Eth2/1/1,2,3-5").as_set == result_correct
 
 
 def test_CiscoRange_15():
     """Basic interface slot range test"""
-    result_correct = [
+    result_correct = {
         "interface Eth 2/1/1",
         "interface Eth 2/1/2",
         "interface Eth 2/1/3",
         "interface Eth 2/1/4",
         "interface Eth 2/1/5",
-    ]
-    assert CiscoRange("interface Eth 2/1/1,2,3-5").as_list == result_correct
+    }
+    assert CiscoRange("interface Eth 2/1/1,2,3-5").as_set == result_correct
 
 
 def test_CiscoRange_16():
     """Empty range test"""
-    result_correct = []
-    assert CiscoRange("").as_list == result_correct
+    result_correct = {""}
+    assert CiscoRange("").as_set == result_correct
 
 
 def test_CiscoRange_17():
     """Append range test"""
-    result_correct = [1, 2, 3]
-    assert CiscoRange("", result_type=int).append("1-3").as_list == result_correct
+    result_correct = {1, 2, 3}
+    assert CiscoRange("", result_type=int).append("1-3").as_set == result_correct
 
 
 def test_CiscoRange_18():
     """Parse a string with a common prefix on all of the CiscoRange() inputs"""
-    result_correct = [
+    result_correct = {
         "Eth1/1",
         "Eth1/10",
         "Eth1/12-20",
-    ]
-    assert CiscoRange("Eth1/1,Eth1/12-20,Eth1/16,Eth1/10").as_list == result_correct
+    }
+    assert CiscoRange("Eth1/1,Eth1/12-20,Eth1/16,Eth1/10").as_set == result_correct
 
 
 def test_CiscoRange_19():
     """Parse a string with a common prefix on all of the CiscoRange() inputs"""
-    result_correct = [
+    result_correct = {
         "interface Eth1/1",
         "interface Eth1/10",
         "interface Eth1/12-20",
-    ]
-    assert CiscoRange("interface Eth1/1,interface Eth1/12-20,interface Eth1/16,interface Eth1/10").as_list == result_correct
+    }
+    assert CiscoRange("interface Eth1/1,interface Eth1/12-20,interface Eth1/16,interface Eth1/10").as_set == result_correct
 
 
 def test_CiscoRange_compressed_str_01():
