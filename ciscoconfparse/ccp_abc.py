a76181fa77d7865894ccbc330dfc554e0f5bcafe
mpenning <mike@pennington.net>
2023-10-15 21:35:56 -0500

    Migrate from 'if False' to 'if 1 == 2'

--- ciscoconfparse/ccp_abc.py
+++ ciscoconfparse/ccp_abc.py
@@ -151,7 +151,7 @@ class BaseCfgLine(metaclass=ABCMeta):
         #     together.
         _line_id = hash(" " * indent + " ".join(self.text.strip().split()))
 
-        if False:
+        if 1 == 2: # Do not execute this code...
             ##################################################################
             # use str.split() below to ensure that whitespace differences
             #     hash the same way... I added this code as a possible
@@ -162,6 +162,7 @@ class BaseCfgLine(metaclass=ABCMeta):
             # I am keeping this in calculate_line_id() to document the
             #     proposal and why I decided against it.
             ##################################################################
+            raise NotImplementedError()
             indent_str = indent * " "
             if self.is_comment is False:
                 _line_id = hash(indent_str + " ".join(self.text.strip().split()))
