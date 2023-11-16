ffe9d57508895b0ddb67378992e2a7161a1bdbbd
mpenning <mike@pennington.net>
2023-11-16 16:43:17 -0600

    Remove name property

--- ciscoconfparse/ccp_abc.py
+++ ciscoconfparse/ccp_abc.py
@@ -174,19 +174,6 @@ class BaseCfgLine(metaclass=ABCMeta):
     # On BaseCfgLine()
     @property
     @logger.catch(reraise=True)
-    def name(self):
-        """subclasses will override this method"""
-        raise NotImplementedError()
-
-    # On BaseCfgLine()
-    @name.setter
-    @logger.catch(reraise=True)
-    def name(self, value):
-        raise NotImplementedError()
-
-    # On BaseCfgLine()
-    @property
-    @logger.catch(reraise=True)
     def is_intf(self):
         """subclasses will override this method"""
         raise NotImplementedError()
